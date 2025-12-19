6e9e4322466574b7ddc7215acdb4e7077bd3e96a
Mor Amiel Rabaev <morku8@gmail.com>
2025-12-19 13:21:53 +0200

    fix editor jsx stuck on loop

--- client/src/components/admin/announcements/TopBar/AddAnc/AddAnc.jsx
+++ client/src/components/admin/announcements/TopBar/AddAnc/AddAnc.jsx
@@ -2,18 +2,22 @@ import React, { useState } from 'react'
 import AddAncForm from './AddAncForm'
 
 function AddAnc() {
-    const [display, setDisplay] = useState(false)
-    
-    return (
-        <div>
-            <button onClick={() => setDisplay(true)}>
-                פרסום חדש
-            </button>
-            <AddAncForm
-            display={display}
-            setDisplay={setDisplay} />
-        </div>
-    )
+  const [display, setDisplay] = useState(false)
+
+  return (
+    <div>
+      <button onClick={() => setDisplay(true)}>
+        פרסום חדש
+      </button>
+
+      {display && (
+        <AddAncForm
+          display={display}
+          setDisplay={setDisplay}
+        />
+      )}
+    </div>
+  )
 }
 
 export default AddAnc
