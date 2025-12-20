397d2edd8ff3b34d99e4948f40e1156a4f04860a
Mor Amiel Rabaev <morku8@gmail.com>
2025-12-21 01:14:42 +0200

    fix steps tree vanished

--- client/src/components/steps/StepsTree/TreeNode.js
+++ client/src/components/steps/StepsTree/TreeNode.js
@@ -1,85 +1,87 @@
-import React, { Fragment, useContext, useEffect, useRef, useState } from 'react';
-import TreeNodeContent from './TreeNodeContent';
-import { useSelector } from 'react-redux';
-import StepsGroup from './StepsGroup';
-import TreeLink from './TreeLink/TreeLink';
-import StepsLevel from './StepsLevel';
-import { getNextSteps, getStepChildren } from '../../../redux/selectors/steps';
-import { StepsContext } from '../StepsContext';
-import useWindowDim from '../../common/useWindowDim';
+import React, { Fragment, useContext, useEffect, useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
 
-function TreeNode({ step, length, duplicateParent }) { 
-    const nextSteps = useSelector(getNextSteps(step?._id))
-    const children = useSelector(getStepChildren(step?._id))
-    const isTopLevel = !step.parent
-    const isGroup = children.length > 0
+import TreeNodeContent from './TreeNodeContent'
+import StepsGroup from './StepsGroup'
+import TreeLink from './TreeLink/TreeLink'
+import StepsLevel from './StepsLevel'
 
-    const isEven = length % 2 === 0
-    const ratio = isEven 
-    ? isGroup ? length : length + 1 
-    : length
+import { getNextSteps, getStepChildren } from '../../../redux/selectors/steps'
+import { StepsContext } from '../StepsContext'
+import useWindowDim from '../../common/useWindowDim'
 
-   const nodeWidth = {
-  flex: '0 0 auto',
-  minWidth: windowWidth <= 650 ? '110px' : '140px'
-}
+function TreeNode({ step, length, duplicateParent }) {
+  /* ---------- hooks that are used by other logic MUST be first ---------- */
+  const { width: windowWidth } = useWindowDim()
+  const ref = useRef(null)
 
-    const {
-        getTreeColor
-    } = useContext(StepsContext)
+  const nextSteps = useSelector(getNextSteps(step?._id)) || []
+  const children = useSelector(getStepChildren(step?._id)) || []
 
-    const color = getTreeColor(step.uniData)
-    const {
-        width: windowWidth
-    } = useWindowDim()
+  const { getTreeColor } = useContext(StepsContext)
 
-    const levelWidth = (windowWidth <= 650) ? 150 : 300
+  /* ---------- derived state ---------- */
+  const isTopLevel = !step?.parent
+  const isGroup = children.length > 0
+  const color = getTreeColor(step?.uniData)
 
-    const width = nextSteps.length === 0 ? levelWidth : '100%'
+  /* ---------- layout ---------- */
+  const nodeWidth = {
+    flex: '0 0 auto',
+    minWidth: windowWidth <= 650 ? '110px' : '140px',
+  }
 
-    const ref = useRef()
+  const levelWidth = windowWidth <= 650 ? 150 : 300
+  const width = nextSteps.length === 0 ? levelWidth : '100%'
 
-    const [divWidth, setDivWidth] = useState(0)
+  /* ---------- link positioning ---------- */
+  const [divWidth, setDivWidth] = useState(0)
 
-    useEffect(() => {
-        setDivWidth(ref.current?.offsetWidth)
-    }, [ref.current, windowWidth, length])
+  useEffect(() => {
+    if (ref.current) {
+      setDivWidth(ref.current.offsetWidth)
+    }
+  }, [windowWidth, length])
 
-    const nodeX = nextSteps.length % 2 === 0  
-        ?   divWidth/(nextSteps.length + 1)
-        :   divWidth/nextSteps.length
-    
-    return (
-        <div
-        ref={ref}
-        style={nodeWidth} 
-        className="tree-node">
-            {isGroup 
-            ?   <StepsGroup
-                parent={step}
-                length={length}
-                color={color}
-                isTopLevel={isTopLevel} />
-            :   <TreeNodeContent
-                duplicateParent={duplicateParent}
-                color={color}
-                step={step} />
-            }
-            {nextSteps?.length > 0 && 
-                <Fragment>
-                    <TreeLink
-                    nodeX={nodeX}
-                    linkWidth={divWidth}
-                    linkInfo={step.linkInfo}
-                    nextSteps={nextSteps}
-                    color={color} />
-                    <StepsLevel
-                    width={width}
-                    nextSteps={nextSteps} />
-                </Fragment>
-            }
-        </div>
-    )
+  const nodeX =
+    nextSteps.length === 0
+      ? 0
+      : nextSteps.length % 2 === 0
+      ? divWidth / (nextSteps.length + 1)
+      : divWidth / nextSteps.length
+
+  /* ---------- render ---------- */
+  return (
+    <div ref={ref} style={nodeWidth} className="tree-node">
+      {isGroup ? (
+        <StepsGroup
+          parent={step}
+          length={length}
+          color={color}
+          isTopLevel={isTopLevel}
+        />
+      ) : (
+        <TreeNodeContent
+          duplicateParent={duplicateParent}
+          color={color}
+          step={step}
+        />
+      )}
+
+      {nextSteps.length > 0 && (
+        <Fragment>
+          <TreeLink
+            nodeX={nodeX}
+            linkWidth={divWidth}
+            linkInfo={step?.linkInfo}
+            nextSteps={nextSteps}
+            color={color}
+          />
+          <StepsLevel width={width} nextSteps={nextSteps} />
+        </Fragment>
+      )}
+    </div>
+  )
 }
 
 export default TreeNode
